692cbfab337fcef1a5f11bbc6c268a0b0cff7560
kemilio <kuchins4@uwm.edu>
2018-01-28 11:50:49 -0600

    windowlocation to window.location

--- infoBoxes.js
+++ infoBoxes.js
@@ -2,9 +2,9 @@ var backButton = document.querySelector('.backButton');
 document.addEventListener('keydown', function(e) {
     key = e.keyCode;
     if (key === 32) {
-      windowlocation = 'afterIndex.html';
+      window.location = 'afterIndex.html';
       }
   })
 backButton.addEventListener('click', function(e) {
-    windowlocation = 'afterIndex.html';
+    window.location = 'afterIndex.html';
     })
