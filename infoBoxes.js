24bb11a0faafc8aa4e03207141cc9747cb58675c
kemilio <kuchins4@uwm.edu>
2018-01-29 09:11:14 -0600

    Got rid of testing response

--- infoBoxes.js
+++ infoBoxes.js
@@ -3,10 +3,8 @@ document.addEventListener('keydown', function(e) {
     key = e.keyCode;
     if (key === 32) {
       window.location = 'afterIndex.html';
-        backButton.textContent = "Yup";
       }
   })
 backButton.addEventListener('click', function(e) {
     window.location = 'afterIndex.html';
-    backButton.textContent = "Puy";
     })
