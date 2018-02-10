f597ef553f542e9089c8bb37b77d5c6e10d5be1a
kemilio <kuchins4@uwm.edu>
2018-02-10 14:27:38 -0600

    Added directory changes

--- JS/infoBoxes.js
+++ JS/infoBoxes.js
@@ -2,9 +2,9 @@ var backButton = document.querySelector('.backButton');
 document.addEventListener('keydown', function(e) {
     key = e.keyCode;
     if (key === 32) {
-      window.location = 'afterIndex.html';
+      window.location = 'HTML/afterIndex.html';
       }
   })
 backButton.addEventListener('click', function(e) {
-    window.location = 'afterIndex.html';
+    window.location = 'HTML/afterIndex.html';
     })
